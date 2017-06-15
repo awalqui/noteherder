72ca7847f33e02889b4c5bd32cdb28ba1ad6ec26
awalqui <walqang@gmail.com>
2017-06-15 10:10:20 -0400

    Everything was vertical for a while but hopefully it's better now

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Sidebar from './Sidebar'
-import Notelist from './Notelist'
-import NoteForm from './NoteForm'
+import Main from './Main'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      notes: {
+        'note-1': {
+          id: 'note-1',
+          title: 'Thoughts on React',
+          body: 'React is pretty nifty. Declarative FTW! 🎸',
+        },
+        'note-2': {
+          id: 'note-2',
+          title: 'State and props?',
+          body: 'Wat',
+        },
+      },
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Sidebar />
-        <Notelist />
-        <NoteForm />
+        <Main notes={this.state.notes} />
       </div>
     );
   }
