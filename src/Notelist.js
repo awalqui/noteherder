38e1bca01d51d2216736e9f6079c83f240f6f0a8
awalqui <walqang@gmail.com>
2017-06-18 18:25:33 -0400

    some JS for removing a note

--- src/Notelist.js
+++ src/Notelist.js
@@ -8,10 +8,16 @@ const NoteList = ({ notes }) => {
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
-        { Object.keys(notes).map((noteId) => {
+        {Object.keys(notes).map((noteId) => {
           return <Note note={notes[noteId]} key={noteId} />
-        }) }
+        })}
       </ul>
+      <button class="remove button alert">
+        <i
+          class="fa fa-trash-o"
+          aria-hidden="true"
+        ></i>
+      </button>
     </div>
   )
 }
